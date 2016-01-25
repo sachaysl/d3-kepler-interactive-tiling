3c7ccf8ffcf9b734a3110f9e2d16e4c4bdc81733
sach <sacha.saint-leger@mail.mcgill.ca>
2016-01-25 15:31:50 -0500

    added 10th mega tile

--- KeplerMonster.jsx
+++ KeplerMonster.jsx
@@ -41,6 +41,11 @@ KeplerMonster = React.createClass({
 	var g9 = g0.append("g")
 		.attr("transform", "scale(0.5, 0.5) translate(210, 0) scale(0.8, 0.8) rotate(54,200,50) translate(1450,-373)");
 
+	var g10 = g.append("g")
+		.attr("transform", "scale(0.5, 0.5) translate(649, -211)");
+
+
+	
 	var point = svg.append("circle")
 		.attr("cx", 200)
 		.attr("cy", 200)
@@ -57,6 +62,7 @@ KeplerMonster = React.createClass({
 	drawKeplerMonster3(g7);
 	drawKeplerMonster3(g8);
 	drawKeplerMonster3(g9);
+	drawKeplerMonster3(g10);
 	
 	
 	function calculatePolygonPoints(sides, centerX, centerY, radius) {
